f8e262b78e22f0da1e839bbf613736a1a9435558
Giovanny Hernandez Arbelaez <46624723+giovanni104@users.noreply.github.com>
2023-05-30 17:29:29 -0500

    avances curso

--- junit5_app/src/test/java/org/gha/junit5app/ejemplos/models/CuentaTest.java
+++ junit5_app/src/test/java/org/gha/junit5app/ejemplos/models/CuentaTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
 
+import org.gha.junit5app.ejemplos.exceptions.DineroInsuficienteException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -58,5 +59,14 @@ class CuentaTest {
 		assertEquals(1100, cuenta.getSaldo().intValue());
 		assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
 	}
-
+	@Test
+	   void testDineroInsuficienteExceptionCuenta() {
+		Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        Exception exception = assertThrows(DineroInsuficienteException.class, () -> {
+            cuenta.debito(new BigDecimal(1500));
+        });
+        String actual = exception.getMessage();
+        String esperado = "Dinero Insuficiente";
+        assertEquals(esperado, actual);
+    }
 }
