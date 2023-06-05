6a8d485ecce781b69b892096fe5f5e4fd57c39aa
Giovanny Hernandez Arbelaez <46624723+giovanni104@users.noreply.github.com>
2023-06-04 22:36:09 -0500

    avances pruebas

--- junit5_app/src/test/java/org/gha/junit5app/ejemplos/models/CuentaTest.java
+++ junit5_app/src/test/java/org/gha/junit5app/ejemplos/models/CuentaTest.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import org.gha.junit5app.ejemplos.exceptions.DineroInsuficienteException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 class CuentaTest {
@@ -17,7 +18,11 @@ class CuentaTest {
 		Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
 		String esperado = "Andres";
 		String real = cuenta.getPersona();
-		assertEquals(esperado, real);
+
+		assertNotNull(real, () -> "la cuenta no puede ser nula");
+		assertEquals(esperado, real, () -> "el nombre de la cuenta no es el que se esperaba: se esperaba " + esperado
+				+ " sin embargo fue " + real);
+		assertTrue(real.equals("Andres"), () -> "nombre cuenta esperada debe ser igual a la real");
 
 	}
 
@@ -59,14 +64,62 @@ class CuentaTest {
 		assertEquals(1100, cuenta.getSaldo().intValue());
 		assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
 	}
+
 	@Test
-	   void testDineroInsuficienteExceptionCuenta() {
+	void testDineroInsuficienteExceptionCuenta() {
 		Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
-        Exception exception = assertThrows(DineroInsuficienteException.class, () -> {
-            cuenta.debito(new BigDecimal(1500));
-        });
-        String actual = exception.getMessage();
-        String esperado = "Dinero Insuficiente";
-        assertEquals(esperado, actual);
-    }
+		Exception exception = assertThrows(DineroInsuficienteException.class, () -> {
+			cuenta.debito(new BigDecimal(1500));
+		});
+		String actual = exception.getMessage();
+		String esperado = "Dinero Insuficiente";
+		assertEquals(esperado, actual);
+	}
+
+	@Test
+	void testTransferirDineroCuentas() {
+		Cuenta cuenta1 = new Cuenta("Jhon Doe", new BigDecimal("2500"));
+		Cuenta cuenta2 = new Cuenta("Andres", new BigDecimal("1500.8989"));
+
+		Banco banco = new Banco();
+		banco.setNombre("Banco del Estado");
+		banco.transferir(cuenta2, cuenta1, new BigDecimal(500));
+		assertEquals("1000.8989", cuenta2.getSaldo().toPlainString());
+		assertEquals("3000", cuenta1.getSaldo().toPlainString());
+	}
+
+	@Test
+	@Tag("cuenta")
+	@Tag("banco")
+	// @Disabled
+	@DisplayName("probando relaciones entre las cuentas y el banco con assertAll.")
+	void testRelacionBancoCuentas() {
+		// fail();
+		Cuenta cuenta1 = new Cuenta("Jhon Doe", new BigDecimal("2500"));
+		Cuenta cuenta2 = new Cuenta("Andres", new BigDecimal("1500.8989"));
+
+		Banco banco = new Banco();
+		banco.addCuenta(cuenta1);
+		banco.addCuenta(cuenta2);
+
+		banco.setNombre("Banco del Estado");
+		banco.transferir(cuenta2, cuenta1, new BigDecimal(500));
+		assertAll(
+				() -> assertEquals("1000.8989", cuenta2.getSaldo().toPlainString(),
+						() -> "el valor del saldo de la cuenta2 no es el esperado."),
+
+				() -> assertEquals("3000", cuenta1.getSaldo().toPlainString(),
+						() -> "el valor del saldo de la cuenta1 no es el esperado."),
+
+				() -> assertEquals(2, banco.getCuentas().size(), () -> "el banco no tienes las cuentas esperadas"),
+
+				() -> assertEquals("Banco del Estado", cuenta1.getBanco().getNombre()),
+
+				() -> assertEquals("Andres",
+						banco.getCuentas().stream().filter(c -> c.getPersona().equals("Andres")).findFirst().get()
+								.getPersona()),
+
+				() -> assertTrue(banco.getCuentas().stream().anyMatch(c -> c.getPersona().equals("Jhon Doe"))));
+	}
+
 }
